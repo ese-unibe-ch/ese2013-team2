e3348947ac19dbc32c93c627fb95523427161adb
leaw <lea.wollensack@gmail.com>
2013-10-14 18:11:10 +0200

    added json stuff
    check MainActivity.java - most important code is under "wichtiger
    codeblock" - hope it is readable

--- Project/android-http-get-json/src/com/hmkcode/android/Sport.java
+++ Project/android-http-get-json/src/com/hmkcode/android/Sport.java
@@ -8,8 +8,15 @@ public class Sport {
 	public Sport(int id, String name){
 		this.id = id;
 		this.name = name;
+	} 
+	
+	public void setID(int id){
+		this.id = id;
 	}
 
+	public void setName(String name){
+		this.name = name;
+	}
 
 	public int getId(){
 		return id;
