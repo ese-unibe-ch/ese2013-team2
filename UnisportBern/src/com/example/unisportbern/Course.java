0950cd3180b27241f753a15b7786cf72aaf10926
Thomas Steinmann <thomas.steinmann@students.unibe.ch>
2013-10-16 16:05:28 +0200

    added course, sport and date

--- UnisportBern/src/com/example/unisportbern/Course.java
+++ UnisportBern/src/com/example/unisportbern/Course.java
@@ -3,5 +3,14 @@ package com.example.unisportbern;
 public class Course {
 
 	// TODO: create class
+	Sport sport;
+	int id;
+	String name;
+	Date date;
+	boolean[] phases;
+	String place;
+	String info;
+	boolean subscribtionRequired;
+	int kew;
 	
 }
