eedaefa46ccd773100510a4819aa0f4d847022a0
Thomas Steinmann <thomas.steinmann@students.unibe.ch>
2013-10-29 14:37:40 +0100

    cleanup

--- UnisportBern/src/ch/unibe/unisportbern/views/CoursesFragment.java
+++ UnisportBern/src/ch/unibe/unisportbern/views/CoursesFragment.java
@@ -17,15 +17,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class CoursesFragment extends ListFragment {
-	
-	public String[] SPORTS;			
+
+	public String[] SPORTS;
 	public final static String EXTRA = "ch.unibe.unisportbern.views";
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
-		//Activate the database
+
+		// Activate the database
 		try {
 			DBMethodes.sportUpdate(getActivity());
 		} catch (JSONException e1) {
@@ -42,51 +42,36 @@ public class CoursesFragment extends ListFragment {
 			e1.printStackTrace();
 		}
 
-		/*Json json = new Json();
-		try {
-			json.getAllCourses(3);
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
-		
-		
-		
-			ArrayList<Sport> sports = DBMethodes.getAllSport(getActivity());
-			
-			
-			//DBMethodes.courseUpdate(getActivity());
-			SPORTS = new String[sports.size()];
-			
-			for(int a=0;a<sports.size();a++){
-				SPORTS[a] = sports.get(a).toString(); 
-			}
+		ArrayList<Sport> sports = DBMethodes.getAllSport(getActivity());
 
+		SPORTS = getNames(sports);
 
-		//ArrayList<Sport> sportList = DBMethodes.getAllSport(getActivity());
+		// ArrayList<Sport> sportList = DBMethodes.getAllSport(getActivity());
 
 		setListAdapter(new ArrayAdapter<String>(getActivity(), R.layout.allcourseslist, SPORTS));
 		ListView list = getListView();
 		list.setTextFilterEnabled(true);
-			list.setOnItemClickListener(new OnItemClickListener() {
+		list.setOnItemClickListener(new OnItemClickListener() {
 
-				@Override
-				public void onItemClick(AdapterView<?> arg0, View arg1, int groupPos, long arg3) {
-					Intent intent = new Intent(getActivity(), DetailsActivity.class);
-					intent.putExtra(EXTRA, SPORTS[groupPos]);
-					startActivity(intent);
-				}
-			});
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int groupPos, long arg3) {
+				Intent intent = new Intent(getActivity(), DetailsActivity.class);
+				intent.putExtra(EXTRA, SPORTS[groupPos]);
+				startActivity(intent);
+			}
+		});
 
 	}
+
+	private String[] getNames(ArrayList<Sport> list) {
+
+		String[] result = new String[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			result[i] = list.get(i).getName();
+		}
+
+		return result;
+	}
+
 }
