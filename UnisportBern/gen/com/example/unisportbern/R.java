87179d7d3c4d1076d4e81a60ae723ca6c3270006
Thomas Steinmann <thomas.steinmann@students.unibe.ch>
2013-11-06 17:03:14 +0100

    git wants me to commit

--- UnisportBern/gen/com/example/unisportbern/R.java
+++ UnisportBern/gen/com/example/unisportbern/R.java
@@ -24,6 +24,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int unisport_logo=0x7f020002;
     }
     public static final class id {
         public static final int CourseDate=0x7f080007;
