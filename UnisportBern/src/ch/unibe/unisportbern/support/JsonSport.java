d5c7732acd1d510cf1f4fd2ef96c9732f1cc9047
Michael Scheurer <michael.scheurer@students.unibe.ch>
2013-11-08 17:24:20 +0100

    jetzt gehts sicher =)

--- UnisportBern/src/ch/unibe/unisportbern/support/JsonSport.java
+++ UnisportBern/src/ch/unibe/unisportbern/support/JsonSport.java
@@ -54,7 +54,7 @@ public class JsonSport extends AsyncTask<Void, Void, Void> {
 
 	public void executeJson() throws JSONException, InterruptedException, ExecutionException, TimeoutException {
 		this.execute();
-		this.get(300,TimeUnit.MILLISECONDS);
+		this.get(500,TimeUnit.MILLISECONDS);
 	}
 
 	private static String convertInputStreamToString(InputStream inputStream) throws IOException {
