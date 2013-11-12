a929f5d1c38d03cfa22716c442ea43e03ed992b3
AndreiChis <andrei@iam.unibe.ch>
2013-11-12 14:16:29 +0100

    removed generated files

--- UnisportBern/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\michael\workspace\unisportbern\bin\AndroidManifest.xml : \
-C:\Users\michael\workspace\unisportbern\AndroidManifest.xml \
--- UnisportBern/bin/MainActivity-instrumented-unaligned.apk.d
+++
@@ -1,5 +0,0 @@
-C:\Users\michael\workspace\unisportbern\bin\MainActivity-instrumented-unaligned.apk : \
-C:\Users\michael\workspace\unisportbern\bin\MainActivity.ap_ \
-C:\Users\michael\workspace\unisportbern\bin\classes.dex \
-C:\Users\michael\workspace\unisportbern\libs\android-support-v4.jar \
-C:\Users\michael\Desktop\API18\sdk\tools\lib\emma_device.jar \
