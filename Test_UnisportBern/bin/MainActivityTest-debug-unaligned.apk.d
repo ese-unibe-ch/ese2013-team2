ad7efe939d18187b785aa7f89b45fe2f79c76258
AndreiChis <andrei@iam.unibe.ch>
2013-11-12 14:18:10 +0100

    removed files from test project

--- Test_UnisportBern/bin/MainActivityTest-debug-unaligned.apk.d
+++
@@ -1,3 +0,0 @@
-C:\Users\michael\workspace\Test_UnisportBern\bin\MainActivityTest-debug-unaligned.apk : \
-C:\Users\michael\workspace\Test_UnisportBern\bin\MainActivityTest.ap_ \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes.dex \
--- Test_UnisportBern/bin/classes.dex.d
+++
@@ -1,8 +0,0 @@
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes.dex : \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\ch\unibe\unisportbern\support\DBMethodesTest.class \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\ch\unibe\unisportbern\support\JsonSportTest.class \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\com\example\unisportbern\test\BuildConfig.class \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\com\example\unisportbern\test\R$attr.class \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\com\example\unisportbern\test\R$drawable.class \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\com\example\unisportbern\test\R$string.class \
-C:\Users\michael\workspace\Test_UnisportBern\bin\classes\com\example\unisportbern\test\R.class \
