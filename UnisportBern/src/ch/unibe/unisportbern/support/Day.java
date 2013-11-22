5545048e401a2a1deb74ab59575ed1286cab877f
Michael Scheurer <michael.scheurer@students.unibe.ch>
2013-11-22 18:00:32 +0100

    Day-Class updated

--- UnisportBern/src/ch/unibe/unisportbern/support/Day.java
+++ UnisportBern/src/ch/unibe/unisportbern/support/Day.java
@@ -1,14 +1,34 @@
 package ch.unibe.unisportbern.support;
+
 import java.util.Calendar;
 
 public class Day {
 	
 	private String[] days;
 	private int dayWanted;
+	private String day;
+	private int nextDay;
+	private int month;
+	private int year;
 	
 	public Day(String day){
 		this.days = new String[]{"So","Mo", "Di", "Mi", "Do", "Fr", "Sa"};
+		this.day = day;
+	}
+	
+	public String getNextDay(){
 		
+		Calendar c = Calendar.getInstance();
+		
+		setDayWanted(this.day);
+		
+		//Difference between today and the day wanted
+		this.controlDateChange(c, this.calculateOffset(c));
+		
+		return this.nextDay + "."+ this.month + "."  + this.year;
+	}
+	
+	private void setDayWanted(String day){
 		for(int a=0;a<7;a++){
 			if(this.days[a].equals(day)){
 				this.dayWanted = a;
@@ -16,45 +36,35 @@ public class Day {
 		}
 	}
 	
-	/*public long getNextDay(){
+	private int calculateOffset(Calendar c){
+		int today = c.get(Calendar.DAY_OF_WEEK);
+		int offset = 0;
 
-		Date date = new Date();
-		int isDay = date.getDay();
-		int offset = 0;
-		
-		if(dayWanted < isDay){
+		if(dayWanted+1 < today){
 			dayWanted += 7;
 		}
 		
-		while(isDay != dayWanted){
+		while(today <= dayWanted){
 			offset ++;
-			isDay ++;
-		}
-				
-		Date newDate = new Date(date.getYear(),date.getMonth(),(date.getDate()+offset));
-		
-		return newDate.getTime();
-	}*/
-	
-	public String getNextDay(){
-		Calendar c = Calendar.getInstance();
-		int isDay = c.get(Calendar.DAY_OF_WEEK);
-		int offset = 0;
-		
-		if(dayWanted < isDay){
-			dayWanted += 7;
+			today ++;
 		}
 		
-		while(isDay <= dayWanted){
-			offset ++;
-			isDay ++;
-		}
-		int dayM= c.get(Calendar.DAY_OF_MONTH)+ offset;
-		
-		return dayM + "."+ c.get(Calendar.MONTH) + "."  + c.get(Calendar.YEAR);
-				
-		
-		
+		return offset;
 	}
-
+	
+	private void controlDateChange(Calendar c, int offset){
+		this.nextDay = c.get(Calendar.DAY_OF_MONTH)+offset;
+		this.month = c.get(Calendar.MONTH);
+		this.year = c.get(Calendar.YEAR);
+		
+		if(this.nextDay+offset >= c.getActualMaximum(Calendar.DAY_OF_MONTH)){
+			this.nextDay = this.nextDay+offset - c.getActualMaximum(Calendar.DAY_OF_MONTH);
+			this.month += 1;
+			
+			if(this.month > 11) {
+				this.year += 1;
+				this.month = 0;
+			}
+		}
+	}
 }
