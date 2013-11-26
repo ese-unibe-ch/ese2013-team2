74dee89dbbcee9407764aee5cfb069349afbe17c
Michael Scheurer <michael.scheurer@students.unibe.ch>
2013-11-26 20:36:09 +0100

    Netzwerkverbindung sauber überprüfen. Auch bei der Map. Absturz verhindert

--- UnisportBern/src/ch/unibe/unisportbern/support/Network.java
+++ UnisportBern/src/ch/unibe/unisportbern/support/Network.java
@@ -1,5 +1,7 @@
 package ch.unibe.unisportbern.support;
 
+import ch.unibe.unisportbern.views.dialogs.StandardMessageDialog;
+import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -17,8 +19,12 @@ public class Network {
 		        (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
 		 
 		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-		return activeNetwork != null &&
-		                      activeNetwork.isConnectedOrConnecting();
+		if(activeNetwork != null && activeNetwork.isConnectedOrConnecting()) return true;
+		else{
+			StandardMessageDialog dialog = new StandardMessageDialog();
+			dialog.show(((Activity)context).getFragmentManager(), "Atention!", "No Network Connection!", "Keine Netzwerkverbindung");
+			return false;
+		}
 	}
 
 }
